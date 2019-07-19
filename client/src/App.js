810e301012dd63d135b91b68ac3f8044d4af4b11
Samantha Olvera <smolvera2010@yahoo.com>
2019-07-19 15:18:35 -0400

    added the react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
