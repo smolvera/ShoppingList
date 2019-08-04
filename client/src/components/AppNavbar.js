16ec1c61ab46fb04d6edf0ec9978c6c43269a264
Samantha Olvera <smolvera2010@yahoo.com>
2019-08-04 14:55:34 -0400

    added register and logout functionality

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -6,9 +6,11 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
+  // NavLink,
   Container
 } from "reactstrap";
+import RegisterModal from "./auth/RegisterModal";
+import Logout from "./auth/Logout";
 
 class AppNavbar extends Component {
   state = {
@@ -30,7 +32,10 @@ class AppNavbar extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/smolvera">Github</NavLink>
+                  <RegisterModal />
+                </NavItem>
+                <NavItem>
+                  <Logout />
                 </NavItem>
               </Nav>
             </Collapse>
